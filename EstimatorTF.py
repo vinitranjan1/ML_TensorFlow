02194376506bd30bd0c7a025103ae9b3f209a36b
Vinit Ranjan <Vinit@Vinits-MacBook-Pro.local>
2017-09-28 02:58:06 -0400

    Fixed estimator

--- EstimatorTF.py
+++ EstimatorTF.py
@@ -6,7 +6,7 @@ feature_columns = [tf.feature_column.numeric_column("x", shape = [1])]
 
 #estimator is interface to invoke training and evaluation
 #following estimator does linear regression in particular
-estimator = tf.estimator.LinearRegressor(feature_columns=feature_columns)
+estimator = tf.estimator.LinearRegressor(feature_columns=feature_columns)#, model_dir = 'var/folders/jl/_rbkn4zd6_g1b2rrhgx058vw0000gn/T/tmpzg9r_944')
 
 x_train = np.array([1., 2., 3., 4.])
 y_train = np.array([0., -1., -2., -3.])
@@ -18,8 +18,10 @@ train_input_fn = tf.estimator.inputs.numpy_input_fn(
     {"x": x_train}, y_train, batch_size=4, num_epochs=1000, shuffle=False)
 eval_input_fn = tf.estimator.inputs.numpy_input_fn(
     {"x": x_eval}, y_eval, batch_size=4, num_epochs=1000, shuffle=False)
-    
+
+estimator.train(input_fn = input_fn, steps = 1000)
+
 train_metrics = estimator.evaluate(input_fn = train_input_fn)
-eval_metric = estimator.evaluate(input_fun = eval_input_fn)
+eval_metrics = estimator.evaluate(input_fn = eval_input_fn)
 print("train metrics: %r"% train_metrics)
 print("eval metrics: %r"% eval_metrics)
